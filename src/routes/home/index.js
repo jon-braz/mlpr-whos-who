37f5f818e62c761e0d247afa7fd9514b2be3043e
Jon Braz <jbraz@outlook.com>
2022-05-20 19:24:34 +0100

    Add title icon

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,6 +3,8 @@ import Header from '../../components/header';
 import OverallMap from '../../components/map';
 import style from './style.scss';
 
+import PigIcon from 'mdi-preact/PigIcon';
+
 const navigateToArea = (area) => {
   route(`/area/${area}`);
 };
@@ -10,6 +12,7 @@ const navigateToArea = (area) => {
 const Home = () => (
   <div class={style.home}>
     <Header addLink={true}>
+      <PigIcon class={style.animalIcon} />
       <h1 class={style.title}>Piggy Map</h1>
     </Header>
     <OverallMap onClick={navigateToArea} />
