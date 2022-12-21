02e685c99ce658fed8de0e8bc8724d48f7a500f5
Jon Braz <jbraz@outlook.com>
2022-12-21 14:49:18 +0000

    Add foundations to display aniamls in an area in a list

--- src/routes/area/index.js
+++ src/routes/area/index.js
@@ -2,9 +2,13 @@ import Header from '../../components/header';
 import { AREAS } from '../../shared/constants';
 import style from './style.scss';
 import AreaMap from '../../components/area-map';
+import FAB from '../../components/fab';
 import { route } from 'preact-router';
-import { useEffect } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import { verifyUserIsLoggedIn } from '../../shared/auth-guard';
+
+import MapIcon from 'mdi-preact/MapIcon';
+import ListIcon from 'mdi-preact/FormatListBulletedIcon';
 
 const Area = ({ area }) => {
   // Protected route - user must be logged in
@@ -12,11 +16,21 @@ const Area = ({ area }) => {
     verifyUserIsLoggedIn();
   }, [area]);
 
+  const [mapView, setMapView] = useState(true);
+
   const areaTitle = AREAS[area].name;
   const areaColor = AREAS[area].color;
 
   const animalOnClick = (animal) => {
     route(`/who/${animal.id}`);
+  };
+
+  const viewMap = () => {
+    setMapView(true);
+  };
+
+  const viewList = () => {
+    setMapView(false);
   };
 
   return (
@@ -27,10 +41,27 @@ const Area = ({ area }) => {
         backgroundColor={areaColor}
         showMenu={true}
       />
-      <AreaMap
-        area={area}
-        showAnimals={true}
-        animalOnClick={animalOnClick}></AreaMap>
+      {mapView ? (
+        <AreaMap
+          area={area}
+          showAnimals={true}
+          animalOnClick={animalOnClick}></AreaMap>
+      ) : (
+        <div>LIST</div>
+      )}
+
+      <FAB>
+        <button
+          onClick={viewMap}
+          class={`${style.iconButton} ${mapView ? style.active : ''}`}>
+          <MapIcon></MapIcon>
+        </button>
+        <button
+          onClick={viewList}
+          class={`${style.iconButton} ${mapView ? '' : style.active}`}>
+          <ListIcon></ListIcon>
+        </button>
+      </FAB>
     </div>
   );
 };
